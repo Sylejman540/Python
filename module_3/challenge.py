8d57589be0ea56dd885d0b5fcd6cbd2abcbe2daa
Sylejman540 <durgutisylejman00@gmail.com>
2025-06-12 19:17:53 +0200

    Update challenge.py

--- module_3/challenge.py
+++ module_3/challenge.py
@@ -2,3 +2,10 @@ costumers = {"Martin", "Jack", "Jones"}
 costumer = "Martin"
 
 print(costumer in costumers)
+
+list_my = ["apples", "oranges" "vehicle"]
+
+my_tuple = (1, 3, 4, 6, 6)
+
+print(list_my)
+print(my_tuple)
